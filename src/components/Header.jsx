7fbe63394daa2287477da888859f04fe2f579384
Enzo <hortramos01@gmail.com>
2025-08-21 09:15:17 -0300

    criando codigo components de header e resultado

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import '../css/global.css'
+import '../css/resultado.css'
+
+
+function Header(){
+    return(
+        <div className="header">
+            <h1>Calculadora IMC</h1>
+        </div>
+    )
+}
+
+export default Header
